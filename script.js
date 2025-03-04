636fdd05adc581494f0711be2aebd4c93a2e6844
Jaylynn <jaylynn.brooks21@gmail.com>
2025-03-04 06:48:19 +0000

    final product- digital nomad with colors

--- script.js
+++ script.js
@@ -1,100 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
-    // House Sales Chart
-    const houseSaleCtx = document.getElementById('houseSaleChart');
-    if (houseSaleCtx) {
-        new Chart(houseSaleCtx, {
+
+    // Remote Worker vs Digital Nomad Chart
+    const remoteWorkerCtx = document.getElementById('remoteWorkerChart');
+    if (remoteWorkerCtx) {
+        new Chart(remoteWorkerCtx, {
             type: 'line',
             data: {
-                labels: ['Mar 2023', 'Jul 2023', 'Dec 2023', 'Mar 2024', 'Jul 2024', 'Dec 2024'],
+                labels: ['2019', '2020', '2021', '2022', '2023', '2024'],
                 datasets: [
-                {
-                    label: 'BC',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: 'rgba(255, 159, 64, 0.2)',
-                    borderColor: 'rgba(255, 159, 64, 1)',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'AB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: 'rgba(255, 159, 64, 0.2)',
-                    borderColor: 'rgba(255, 159, 64, 1)',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'SK',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: 'rgba(255, 159, 64, 0.2)',
-                    borderColor: 'rgba(255, 159, 64, 1)',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'MB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: 'rgba(255, 159, 64, 0.2)',
-                    borderColor: 'rgba(255, 159, 64, 1)',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'ON',
-                    data: [881946, 856239, 853195, 889103, 867835, 834123],
-                    backgroundColor: 'rgba(104, 75, 192, 0.2)',
-                    borderWidth: 1,
-                    borderColor: 'rgba(44, 113, 113, 0.2)',
-
-                },
-                {
-                    label: 'QC',
-                    data: [40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90],
-                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
-                    borderColor: 'rgba(153, 102, 255, 1)',
-                    borderWidth: 1,
-                    borderColor: 'black',
-
-                },
-                {
-                    label: 'NL',
-                    data: [40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90],
-                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
-                    borderColor: 'rgba(153, 102, 255, 1)',
-                    borderWidth: 1,
-                    borderColor: 'black',
-
-                },
-                {
-                    label: 'PEI',
-                    data: [40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90],
-                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
-                    borderColor: 'rgba(153, 102, 255, 1)',
-                    borderWidth: 1,
-                    borderColor: 'black',
-
-                },
-                {
-                    label: 'NB',
-                    data: [40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90],
-                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
-                    borderColor: 'rgba(153, 102, 255, 1)',
-                    borderWidth: 1,
-                    borderColor: 'black',
-
-                },
-                {
-                    label: 'NS',
-                    data: [40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90],
-                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
-                    borderColor: 'rgba(153, 102, 255, 1)',
-                    borderWidth: 1,
-                    borderColor: 'black',
-
-                },
-
-
+                    {
+                        label: 'Remote Workers',
+                        data: [3.2, 6.3, 10.2, 11.1, 10.7, 10.2],
+                        backgroundColor: 'rgba(255, 242, 64, 0.64)',
+                        borderColor: 'rgba(255, 242, 64, 0.64)',
+                        borderWidth: 1,
+                        borderColor: 'black',
+                    },
+                    {
+                        label: 'Digital Nomads',
+                        data: [4.1, 4.6, 5.3, 5.8, 6.6, 7.9],
+                        backgroundColor: '#5faa9a',
+                        borderColor: '#5faa9a',
+                        borderWidth: 1,
+                        borderColor: 'black',
+                    },
                 ]
             },
             options: {
@@ -103,13 +32,13 @@ document.addEventListener('DOMContentLoaded', function () {
                         beginAtZero: true,
                         title: {
                             display: true,
-                            text: 'Sale Price (in thousands)'
+                            text: 'Number of Workers (in millions)',
                         }
                     },
                     x: {
                         title: {
                             display: true,
-                            text: 'Date'
+                            text: 'Year'
                         }
                     }
                 }
@@ -117,86 +46,30 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // Avg 2 Bed Rent Chart
-    const rentTwoBedCtx = document.getElementById('rentTwoBedChart');
-    if (rentTwoBedCtx) {
-        new Chart(rentTwoBedCtx, {
+    // Age Distribution Chart
+    const ageCtx = document.getElementById('ageChart');
+    if (ageCtx) {
+        new Chart(ageCtx, {
             type: 'bar',
             data: {
-                labels: ['BC', 'AB', 'SK', '2011'],
+                labels: ['Gen Z', 'Millennials', 'Gen X', 'Boomers'],
                 datasets: [{
-                    label: 'BC',
-                    data: [100, 200, 300, 400],
-                    backgroundColor: '#B5465D',
+                    label: '2023',
+                    data: [21, 37, 27, 15],
+                    backgroundColor: '#feeee4',
+                    borderColor: '#feeee4',
                     borderWidth: 1,
                     borderColor: 'black',
                 },
                 {
-                    label: 'AB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: '#B5465D',
+                    label: '2024',
+                    data: [26, 38, 25, 11],
+                    backgroundColor: '#f37341',
+                    borderColor: '#f37341',
                     borderWidth: 1,
                     borderColor: 'black',
                 },
-                {
-                    label: 'SK',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: '#B5465D',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'AB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: '#B5465D',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'AB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: '#B5465D',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'AB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: '#B5465D',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'AB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: '#B5465D',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'AB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: '#B5465D',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'AB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: '#B5465D',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-                {
-                    label: 'AB',
-                    data: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
-                    backgroundColor: '#B5465D',
-                    borderWidth: 1,
-                    borderColor: 'black',
-                },
-        
-                
-            ]
+                ]
             },
 
             options: {
@@ -205,98 +78,118 @@ document.addEventListener('DOMContentLoaded', function () {
                         beginAtZero: true,
                         title: {
                             display: true,
-                            text: 'Monthly Rent (in hundreds)'
+                            text: 'Percentage of Age Distribution',
                         }
                     },
-                    
+
                 }
             }
         });
     }
 
-    const pieCtx = document.getElementById('myPieChart');
-    if (pieCtx) {
-        new Chart(pieCtx, {
+    // FT vs PT Chart
+    const fullTimeCtx = document.getElementById('fullTimeChart');
+    if (fullTimeCtx) {
+        new Chart(fullTimeCtx, {
             type: 'pie',
             data: {
-                labels: ['English', 'French', 'Other', 'Prefer not to say'],
+                labels: ['Full-Time', 'Part-Time',],
                 datasets: [{
-                    label: 'Languages',
-                    data: [50, 25, 15, 10],
+                    label: 'FT vs PT',
+                    data: [64, 36],
                     borderWidth: 1
-                }]
+                },
+
+                ]
             }
         });
     }
 
-    const anotherPieCtx = document.getElementById('anotherPieChart');
-    if (anotherPieCtx) {
-        new Chart(anotherPieCtx, {
+    // Education Level Chart
+    const educationCtx = document.getElementById('educationChart');
+    if (educationCtx) {
+        new Chart(educationCtx, {
             type: 'pie',
             data: {
-                labels: ['Category A', 'Category B', 'Category C', 'Category D'],
+                labels: ['College Degree', 'Advanced Degree',],
                 datasets: [{
                     label: 'Categories',
-                    data: [30, 40, 20, 10],
-                    borderWidth: 1
+                    data: [52, 18],
+                    borderWidth: 1,
+                    
                 }]
             }
         });
     }
 
-    const extraPieCtx = document.getElementById('extraPieChart');
-    if (extraPieCtx) {
-        new Chart(extraPieCtx, {
-            type: 'pie',
-            data: {
-                labels: ['Segment 1', 'Segment 2', 'Segment 3', 'Segment 4'],
-                datasets: [{
-                    label: 'Segments',
-                    data: [25, 35, 20, 20],
-                    borderWidth: 1
-                }]
-            }
-        });
-    }
-    const extraBarCtx = document.getElementById('extraBarChart');
-    if (extraBarCtx) {
-        new Chart(extraBarCtx, {
+    // Challenges Chart
+    const challengesCtx = document.getElementById('challengesChart');
+    if (challengesCtx) {
+        new Chart(challengesCtx, {
             type: 'bar',
             data: {
-                labels: ['Q1', 'Q2', 'Q3', 'Q4'],
-                datasets: [{
-                    label: 'Quarterly Revenue',
-                    data: [150, 200, 250, 300],
-                    borderWidth: 1
-                }]
+                labels: ['Financial Stress', 'Personal Safety', 'Being Away From Family', 'Time Zone Issues', 'Managing Work & Travel', 'Loneliness', 'Travel Logistics'],
+                datasets: [
+                    {
+                        label: '2023',
+                        data: [27, 22, 26, 23, 24, 16, 21],
+                        backgroundColor: '#85c2e6',
+                        borderColor: '#85c2e6',
+                        borderWidth: 1,
+                        borderColor: 'black',
+                    },
+
+                    {
+                        label: '2024',
+                        data: [25, 19, 28, 24, 23, 18, 23],
+                        backgroundColor: '#6ea386',
+                        borderColor: '#6ea386',
+                        borderWidth: 1,
+                        borderColor: 'black',
+                    },
+                ]
             },
             options: {
                 scales: {
                     y: {
-                        beginAtZero: true
-                    }
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            text: 'Percentage of Top Challenges',
+                        }
+                    },
+
                 }
             }
         });
     }
 
-    const anotherLineCtx = document.getElementById('anotherLineChart');
-    if (anotherLineCtx) {
-        new Chart(anotherLineCtx, {
-            type: 'line',
+    // Industries Chart
+    const industriesCtx = document.getElementById('industriesChart');
+    if (industriesCtx) {
+        new Chart(industriesCtx, {
+            type: 'bar',
             data: {
-                labels: ['2015', '2016', '2017', '2018'],
+                labels: ['IT', 'Creative', 'Education', 'Sales Marketing & PR', 'Finance & Accounting', 'Consulting', 'Research'],
                 datasets: [{
-                    label: 'Growth',
-                    data: [50, 100, 150, 200],
-                    borderWidth: 1
+                    label: '2024',
+                    data: [19, 14, 9, 8, 7, 7, 6],
+                    backgroundColor: '#feedee',
+                    borderColor: '#feedee',
+                    borderWidth: 1,
+                    borderColor: 'black',
                 }]
             },
             options: {
                 scales: {
                     y: {
-                        beginAtZero: true
-                    }
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            text: 'Percentage of Digital Nomads by Industry',
+                        }
+                    },
+
                 }
             }
         });
